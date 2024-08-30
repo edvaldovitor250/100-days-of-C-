bd944706a03fd70a6b8cea0499c169a7f914e806
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-08-30 10:36:07 -0300

    Day #008 complete

--- challenges/Day003/obj/Debug/net8.0/Day003.AssemblyInfo.cs
+++ challenges/Day003/obj/Debug/net8.0/Day003.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day003")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9c8c2b2e4fcc22081ad7d86f729bdaeefbae54c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf8eefca142e891c788f8fa0f13228cc5862c1aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day003")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day003")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ challenges/Day008/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("Escolha a operação: ");
+string op = Console.ReadLine();
+
+Console.WriteLine("Escolha um numero: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Escolha escolha o segundo numero: ");
+int num2 = int.Parse(Console.ReadLine());
+
+switch (op)
+{
+    case "+":
+    {
+      int  soma = num1 + num2;
+        Console.WriteLine(soma);
+          break;
+    }
+    case "-":
+    {
+       int sub = num1 + num2;
+        Console.WriteLine(sub);
+          break;
+    }
+     case "*":
+    {
+      int  mult = num1 * num2;
+        Console.WriteLine(mult);
+          break;
+    }
+     case "/":
+    {
+       int div = num1 / num2;
+        Console.WriteLine(div);
+          break;
+    }
+    
+    default:
+    {
+        Console.WriteLine("Sua operação está errada, escolha uma válida: + - / *");
+        break;
+    }
+    }
+
