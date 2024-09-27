3ca9d4bf4562499e797e90058951e8050fe87ac2
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-09-27 11:55:53 -0300

    Day #036 complete

--- challenges/Day006/obj/Debug/net8.0/Day006.AssemblyInfo.cs
+++ challenges/Day006/obj/Debug/net8.0/Day006.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day006")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fcfc0820cc92b34f7fe59dc22a8cc0695502f90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faf657ff60e29345507ae90fe3e8a02b5a7da048")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day006")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day006")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ challenges/Day036/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Edge
+{
+    public int Source, Destination, Weight;
+
+    public Edge(int source, int destination, int weight)
+    {
+        Source = source;
+        Destination = destination;
+        Weight = weight;
+    }
+}
+
+public class Graph
+{
+    public int VerticesCount;
+    public List<Edge> Edges;
+
+    public Graph(int verticesCount)
+    {
+        VerticesCount = verticesCount;
+        Edges = new List<Edge>();
+    }
+
+    public void AddEdge(int source, int destination, int weight)
+    {
+        Edges.Add(new Edge(source, destination, weight));
+    }
+
+    public void BellmanFord(int startVertex)
+    {
+        int[] distances = new int[VerticesCount];
+        for (int i = 0; i < VerticesCount; i++)
+        {
+            distances[i] = int.MaxValue;
+        }
+        distances[startVertex] = 0;
+
+        for (int i = 1; i < VerticesCount; i++)
+        {
+            foreach (var edge in Edges)
+            {
+                int u = edge.Source;
+                int v = edge.Destination;
+                int weight = edge.Weight;
+
+                if (distances[u] != int.MaxValue && distances[u] + weight < distances[v])
+                {
+                    distances[v] = distances[u] + weight;
+                }
+            }
+        }
+
+        foreach (var edge in Edges)
+        {
+            int u = edge.Source;
+            int v = edge.Destination;
+            int weight = edge.Weight;
+
+            if (distances[u] != int.MaxValue && distances[u] + weight < distances[v])
+            {
+                Console.WriteLine("Graph contains a negative-weight cycle");
+                return;
+            }
+        }
+
+        PrintDistances(distances);
+    }
+
+    private void PrintDistances(int[] distances)
+    {
+        Console.WriteLine("Vertex Distance from Source");
+        for (int i = 0; i < VerticesCount; i++)
+        {
+            Console.WriteLine($"{i}\t\t{distances[i]}");
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int verticesCount = 5;
+        Graph graph = new Graph(verticesCount);
+
+        graph.AddEdge(0, 1, -1);
+        graph.AddEdge(0, 2, 4);
+        graph.AddEdge(1, 2, 3);
+        graph.AddEdge(1, 3, 2);
+        graph.AddEdge(1, 4, 2);
+        graph.AddEdge(3, 2, 5);
+        graph.AddEdge(3, 1, 1);
+        graph.AddEdge(4, 3, -3);
+
+        graph.BellmanFord(0); 
+    }
+}
