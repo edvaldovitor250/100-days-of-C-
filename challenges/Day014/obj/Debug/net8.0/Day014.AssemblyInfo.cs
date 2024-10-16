d9dd2d75a7c347acf625f8ceed3bb42966827552
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-10-16 10:21:28 -0300

    Day #055 complete

--- challenges/Day014/obj/Debug/net8.0/Day014.AssemblyInfo.cs
+++ challenges/Day014/obj/Debug/net8.0/Day014.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day014")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa5bf311f12a049211a14d36e4b7456f62500485")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35c761bbb6972a8b8c739ab21ab4bb654edbd3f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day014")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day014")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ challenges/Day055/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+class Chat
+{
+    private static List<string> messages = new List<string>(); 
+
+    public void SendMessage(string user, string message)
+    {
+        lock (messages)
+        {
+            messages.Add($"{user}: {message}");
+        }
+    }
+
+    public void DisplayMessages()
+    {
+        lock (messages)
+        {
+            Console.Clear();
+            foreach (var message in messages)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
+
+class Program
+{
+    static Chat chat = new Chat();
+    static string user;
+
+    static void Main(string[] args)
+    {
+        Console.Write("Digite seu nome: ");
+        user = Console.ReadLine();
+
+        Thread thread = new Thread(ListenForMessages); 
+        thread.Start();
+
+        while (true)
+        {
+            string message = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(message))
+            {
+                chat.SendMessage(user, message); 
+            }
+        }
+    }
+
+    static void ListenForMessages()
+    {
+        while (true)
+        {
+            chat.DisplayMessages(); 
+            Thread.Sleep(1000);
+        }
+    }
+}
