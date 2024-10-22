04770ae1f25c8cfb57cdd24b1cc7331853b2a86f
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-10-22 10:51:01 -0300

    Day #061 complete

--- challenges/Day014/obj/Debug/net8.0/Day014.AssemblyInfo.cs
+++ challenges/Day014/obj/Debug/net8.0/Day014.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day014")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6e55ae27c3c21bdd0b37698068f4d5e424a98fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80512f54b190b03840125c3c443ff7fb01dd3610")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day014")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day014")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ challenges/Day061/Program.cs
@@ -0,0 +1,36 @@
+﻿public class RecomendacaoFilmes{
+
+    string[] filmes = { "O Poderoso Chefão",
+            "Star Wars",
+            "O Senhor dos Anéis",
+            "Matrix",
+            "Pulp Fiction",
+            "Gladiador",
+            "Inception",
+            "O Lobo de Wall Street",
+            "Interestelar",
+            "Coringa"};
+    double[] pontuacoes = {3,4,2,6,5,3,5,2,10};
+
+    public string ObterRecomendacao(double pontuacao)
+    {
+        int index = Array.IndexOf(pontuacoes, pontuacao);
+        if (index != -1) 
+        {
+            return filmes[index];
+        }
+        else
+        {
+            return "Nenhum filme encontrado com essa pontuação.";
+        }
+    }
+
+public static void Main(string[] args)
+{
+    RecomendacaoFilmes recomendacao = new RecomendacaoFilmes();
+    double pontuacao = 3;
+    Console.WriteLine("Recomendado: " + recomendacao.ObterRecomendacao(pontuacao));
+}
+    
+
+}
