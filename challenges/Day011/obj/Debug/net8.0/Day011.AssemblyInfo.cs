472d51f922bfe8a86e0d91c950fb5000453860b8
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-10-30 10:18:02 -0300

    Day #069 complete

--- challenges/Day011/obj/Debug/net8.0/Day011.AssemblyInfo.cs
+++ challenges/Day011/obj/Debug/net8.0/Day011.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day011")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+579493599a49610c95a37a7f5af28e9db54ebece")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e312228b572dece9633613db62750880bcac3ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day011")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day011")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ challenges/Day069/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class GerenciamentoProjetos
+{
+    List<string> projetos = new List<string>();
+    List<string> versaos = new List<string>();
+    List<string> descricaos = new List<string>();
+
+    public void AdicionarProjeto(string nome, string versao, string descricao)
+    {
+        projetos.Add(nome);
+        versaos.Add(versao);
+        descricaos.Add(descricao);
+    }
+
+    public void EditarProjeto(int index, string upname, string upversao, string updescricao)
+    {
+        if (index >= 0 && index < projetos.Count)
+        {
+            projetos[index] = upname;
+            versaos[index] = upversao;
+            descricaos[index] = updescricao;
+        }
+        else
+        {
+            Console.WriteLine("Índice de projeto inválido.");
+        }
+    }
+
+    public void VerProgresso()
+    {
+        Console.WriteLine("Progresso dos projetos:");
+        for (int i = 0; i < projetos.Count; i++)
+        {
+            Console.WriteLine($"{projetos[i]} - Versão: {versaos[i]}, Descrição: {descricaos[i]}");
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        GerenciamentoProjetos gerenciamento = new GerenciamentoProjetos();
+
+        gerenciamento.AdicionarProjeto("Projeto A", "1.0", "Descrição do Projeto A");
+        gerenciamento.AdicionarProjeto("Projeto B", "1.1", "Descrição do Projeto B");
+
+        Console.WriteLine("Antes da edição:");
+        gerenciamento.VerProgresso();
+
+        gerenciamento.EditarProjeto(0, "Projeto A Editado", "2.0", "Descrição Atualizada do Projeto A");
+
+        Console.WriteLine("\nDepois da edição:");
+        gerenciamento.VerProgresso();
+    }
+}
