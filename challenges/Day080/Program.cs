826c15c1f22060ae69cf0bfa12a3ed4910e0ccf5
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-11-10 08:04:04 -0300

    Day #080 complete

---
+++ challenges/Day080/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class GerenciamnetoParque
+{
+    List<string> ingressos = new List<string>();
+    List<string> atracoes = new List<string>();
+
+    public void AdicionarIngresso(string ingresso)
+    {
+        ingressos.Add(ingresso);
+    }
+
+    public void AdicionarAtracoes(string a)
+    {
+        atracoes.Add(a);
+    }
+
+    public void ListarIngressos()
+    {
+        foreach (var item in ingressos)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    public void ListarAtracoes()
+    {
+        foreach (var item in atracoes)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        GerenciamnetoParque gerenciador = new GerenciamnetoParque();
+
+        gerenciador.AdicionarIngresso("Ingresso VIP");
+        gerenciador.AdicionarIngresso("Ingresso Normal");
+
+        gerenciador.AdicionarAtracoes("Montanha Russa");
+        gerenciador.AdicionarAtracoes("Roda Gigante");
+
+        Console.WriteLine("Ingressos Disponíveis:");
+        gerenciador.ListarIngressos();
+
+        Console.WriteLine("\nAtrações Disponíveis:");
+        gerenciador.ListarAtracoes();
+    }
+}
