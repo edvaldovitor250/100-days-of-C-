8d53e8db3e3d0e2af538123d7b1866e2f17598f5
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-11-11 21:34:52 -0300

    Day #081 complete

--- challenges/Day010/obj/Debug/net8.0/Day010.AssemblyInfo.cs
+++ challenges/Day010/obj/Debug/net8.0/Day010.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day010")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b39571a66295d2b59d2dfd9a8ceddb2b3d5dbac1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b769ab539483c681d06da1d959f79c8cb39a2099")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day010")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day010")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ challenges/Day081/Program.cs
@@ -0,0 +1,59 @@
+﻿public class GerenciamentoRestaurante
+{
+    List<string> mesas = new List<string>();
+    List<string> pedidos = new List<string>();
+    List<int> pagamentos = new List<int>();
+
+    public void gerenciamentoMesa(string mesa, string pedido,int pagamento)
+    {
+        mesas.Add(mesa);
+        pedidos.Add(pedido);
+        pagamentos.Add(pagamento);
+        Console.WriteLine($"Pedido '{pedido}' servido para a mesa '{mesa}' e pago com '{pagamento}'.");
+    }
+
+    public void gerenciamentoPedido(string pedido, string mesa)
+    {
+        if (mesas.Contains(mesa))
+        {
+            int index = mesas.IndexOf(mesa);
+            pedidos[index] = pedido;
+            Console.WriteLine($"Pedido '{pedido}' atualizado para a mesa '{mesa}'.");
+        }
+        else
+        {
+            Console.WriteLine($"Mesa '{mesa}' não encontrada.");
+        }
+    }
+
+    public void gerenciarPagamento(string pedido, int pagamento)
+    {
+        int index = pedidos.IndexOf(pedido);
+        if (index >= 0)
+        {
+            pagamentos[index] = pagamento;
+            Console.WriteLine($"Pagamento de '{pagamento}' para o pedido '{pedido}' confirmado.");
+        }
+        else
+        {
+            Console.WriteLine($"Pedido '{pedido}' não encontrado.");
+        }
+    }
+
+    public static void Main()
+    {
+        GerenciamentoRestaurante restaurante = new GerenciamentoRestaurante();
+
+        restaurante.gerenciamentoMesa("Mesa 1", "Pizza", 50);
+        restaurante.gerenciamentoMesa("Mesa 2", "Pasta", 30);
+        restaurante.gerenciamentoMesa("Mesa 3", "Salada", 20);
+
+        restaurante.gerenciamentoPedido("Sushi", "Mesa 1");
+
+        restaurante.gerenciarPagamento("Sushi", 60);
+
+        restaurante.gerenciamentoPedido("Bebida", "Mesa 4");
+
+        restaurante.gerenciarPagamento("Sobremesa", 15);
+    }
+}
