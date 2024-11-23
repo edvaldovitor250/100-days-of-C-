bad56fe72d64aba6a4f59abd5c48f4a76dd4dcc2
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-11-22 22:31:52 -0300

    Day #092 complete

--- challenges/Day009/obj/Debug/net8.0/Day009.AssemblyInfo.cs
+++ challenges/Day009/obj/Debug/net8.0/Day009.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day009")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81bf1c806000e4df474c955324406452e39a86b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27b4b3b896c44533ab786fdb63e178dbb964d4df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day009")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day009")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ challenges/Day092/README.md
@@ -0,0 +1,10 @@
+# Day 92
+
+## Desafio:
+
+	Implemente um sistema em C# para gerenciar uma empresa de catering, com funcionalidades para gerenciar eventos, cardápios e clientes.
+
+**Resultado:**
+
+```cshap
+
